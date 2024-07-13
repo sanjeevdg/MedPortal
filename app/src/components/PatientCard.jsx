041f9fbcae8567d5ef14baf34f39ea7c3160d3f2
Dasarath G <dasarathg68@gmail.com>
2024-07-13 22:03:06 +0530

    style: add padding

--- app/src/components/PatientCard.jsx
+++ app/src/components/PatientCard.jsx
@@ -1,6 +1,6 @@
 const PatientCard = ({ patient }) => {
   return (
-    <div className="flex flex-row space-x-4">
+    <div className="flex flex-row space-x-4 p-4">
       <div className="card border shadow-2xl rounded-2xl w-72 h-32 justify-start">
         <span className="card-title ">{patient.name}</span>
         <span className="text-xs text-left">{patient.email}</span>
