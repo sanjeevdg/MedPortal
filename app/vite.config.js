85d68698fbb1794ec488cdefd51d734fd0342487
Dasarath G <dasarathg68@gmail.com>
2024-07-14 00:11:31 +0530

    fix: vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss("./tailwind.config.js")],
+  plugins: [react()],
   define: {
     global: {},
     __APP_ENV__: process.env.VITE_VERCEL_ENV,
   },
-
   optimizeDeps: {
     disabled: false,
   },
