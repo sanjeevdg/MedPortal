fc9262eff9f16285852018527b47edeb2cb23f2c
Dasarath G <dasarathg68@gmail.com>
2024-07-13 15:02:00 +0530

    feat: add name

--- app/src/components/Register.jsx
+++ app/src/components/Register.jsx
@@ -75,6 +75,23 @@ const Register = () => {
                   <span className="ml-2">Doctor</span>
                 </label>
               </div>
+              <label className="input input-bordered flex items-center gap-2 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input
+                  type="text"
+                  name="name"
+                  className="grow"
+                  placeholder="Enter name"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
 
               <label className="input input-bordered flex items-center gap-2 mb-4">
                 <svg
