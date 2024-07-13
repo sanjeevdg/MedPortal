5bc3a2aa4d3f44733eb9c4c8cdd26ec585952fc2
Dasarath G <dasarathg68@gmail.com>
2024-07-13 23:53:39 +0530

    ci/cd

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -9,7 +9,9 @@ import {
 } from "../controllers/doctorController.js";
 import { authorizeUser } from "../middleware/middleware.js";
 const doctorRoutes = express.Router();
-
+doctorRoutes.get("/", (req, res) => {
+  res.send("Everything okay").status(200);
+});
 doctorRoutes.post("/linkpatient", authorizeUser, linkPatientToDoctor);
 doctorRoutes.get("/patients", authorizeUser, getAllPatients);
 doctorRoutes.get("/patients/search", authorizeUser, searchPatients);
